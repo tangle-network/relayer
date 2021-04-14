9e760624ffe401515c7683fb8458ba838147c8d9
Shady Khalifa <shekohex@gmail.com>
2021-04-14 18:20:29 +0200

    add RPC crates

--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use ws::WsServer;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::try_init_from_env("WEBB_LOG")?;
+    let mut server = WsServer::new("0.0.0.0:9933").await?;
+    server.register_method("webb_Relay", |_| Ok("Todo!"))?;
+    println!("Starting on port 9933");
+    server.start().await;
+    Ok(())
 }
