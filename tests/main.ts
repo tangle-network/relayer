3ac770136009e324524c889663e960336149d19a
Shady Khalifa <shekohex@gmail.com>
2021-07-02 05:00:30 +0200

    add integration testing

---
+++ tests/main.ts
@@ -0,0 +1,308 @@
+import circomlib from 'circomlib';
+import crypto from 'crypto';
+import { ethers } from 'ethers';
+import fs from 'fs';
+import WebSocket from 'ws';
+import ganache from 'ganache-cli';
+import snarkjs from 'snarkjs';
+import websnarkUtils from 'websnark/src/utils';
+import buildGroth16 from 'websnark/src/groth16';
+import anchorContract from './build/contracts/Anchor.json';
+import nativeAnchorContract from './build/contracts/NativeAnchor.json';
+import verifierContract from './build/contracts/Verifier.json';
+import MerkleTree from './lib/MerkleTree';
+import { spawn } from 'child_process';
+
+const PRIVATE_KEY =
+  '0x000000000000000000000000000000000000000000000000000000000000dead';
+const web3Provider = ganache.provider({
+  accounts: [
+    {
+      balance: ethers.utils.parseEther('1000').toHexString(),
+      secretKey: PRIVATE_KEY,
+    },
+  ],
+  ws: false,
+  port: 1998,
+});
+
+const provider = new ethers.providers.Web3Provider(web3Provider);
+const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+const toHex = (number: number | Buffer, length = 32) =>
+  '0x' +
+  (number instanceof Buffer
+    ? number.toString('hex')
+    : snarkjs.bigInt(number).toString(16)
+  ).padStart(length * 2, '0');
+
+async function deployNativeAnchor() {
+  const genContract = require('circomlib/src/mimcsponge_gencontract.js');
+
+  const hasherContractRaw = {
+    contractName: 'Hasher',
+    abi: genContract.abi,
+    bytecode: genContract.createCode('mimcsponge', 220),
+  };
+
+  const verifierContractRaw = {
+    contractName: 'Verifier',
+    abi: verifierContract.abi,
+    bytecode: verifierContract.bytecode,
+  };
+
+  const nativeAnchorContractRaw = {
+    contractName: 'NativeAnchor',
+    abi: nativeAnchorContract.abi,
+    bytecode: nativeAnchorContract.bytecode,
+  };
+
+  const hasherFactory = new ethers.ContractFactory(
+    hasherContractRaw.abi,
+    hasherContractRaw.bytecode,
+    wallet
+  );
+  let hasherInstance = await hasherFactory.deploy({ gasLimit: '0x5B8D80' });
+  await hasherInstance.deployed();
+
+  const verifierFactory = new ethers.ContractFactory(
+    verifierContractRaw.abi,
+    verifierContractRaw.bytecode,
+    wallet
+  );
+  let verifierInstance = await verifierFactory.deploy({ gasLimit: '0x5B8D80' });
+  await verifierInstance.deployed();
+
+  const denomination = ethers.utils.parseEther('0.1');
+  const merkleTreeHeight = 20;
+  const nativeAnchorFactory = new ethers.ContractFactory(
+    nativeAnchorContractRaw.abi,
+    nativeAnchorContractRaw.bytecode,
+    wallet
+  );
+  let nativeAnchorInstance = await nativeAnchorFactory.deploy(
+    verifierInstance.address,
+    hasherInstance.address,
+    denomination,
+    merkleTreeHeight,
+    { gasLimit: '0x5B8D80' }
+  );
+  const nativeAnchorAddress = await nativeAnchorInstance.deployed();
+
+  return nativeAnchorAddress.address;
+}
+
+function createDeposit() {
+  const rbigint = (nbytes) =>
+    snarkjs.bigInt.leBuff2int(crypto.randomBytes(nbytes));
+  const pedersenHash = (data) =>
+    circomlib.babyJub.unpackPoint(circomlib.pedersenHash.hash(data))[0];
+  let deposit: any = { nullifier: rbigint(31), secret: rbigint(31) };
+  deposit.preimage = Buffer.concat([
+    deposit.nullifier.leInt2Buff(31),
+    deposit.secret.leInt2Buff(31),
+  ]);
+  deposit.commitment = pedersenHash(deposit.preimage);
+  deposit.nullifierHash = pedersenHash(deposit.nullifier.leInt2Buff(31));
+  return deposit;
+}
+
+async function deposit(contractAddress: string) {
+  const deposit = createDeposit();
+  const nativeAnchorInstance = new ethers.Contract(
+    contractAddress,
+    nativeAnchorContract.abi,
+    wallet
+  );
+  const toFixedHex = (number: number | Buffer, length = 32) =>
+    '0x' +
+    (number instanceof Buffer
+      ? number.toString('hex')
+      : snarkjs.bigInt(number).toString(16)
+    ).padStart(length * 2, '0');
+
+  const denomination = await nativeAnchorInstance.functions.denomination!();
+  await nativeAnchorInstance.deposit(toFixedHex(deposit.commitment), {
+    value: denomination.toString(),
+  });
+  return deposit;
+}
+
+async function getDepositEvents(contractAddress: string) {
+  // Query the blockchain for all deposits that have happened
+  const anchorInterface = new ethers.utils.Interface(anchorContract.abi);
+  const anchorInstance = new ethers.Contract(
+    contractAddress,
+    anchorContract.abi,
+    provider
+  );
+  const depositFilterResult = anchorInstance.filters.Deposit!();
+  const currentBlock = await provider.getBlockNumber();
+
+  const logs = await provider.getLogs({
+    fromBlock: currentBlock - 1000,
+    toBlock: currentBlock,
+    address: contractAddress,
+    //@ts-ignore
+    topics: [depositFilterResult.topics],
+  });
+
+  // Decode the logs for deposit events
+  const decodedEvents = logs.map((log) => {
+    return anchorInterface.parseLog(log);
+  });
+
+  return decodedEvents;
+}
+
+async function generateMerkleProof(deposit: any, contractAddress: string) {
+  const events = await getDepositEvents(contractAddress);
+  const leaves = events
+    .sort((a, b) => a.args.leafIndex - b.args.leafIndex) // Sort events in chronological order
+    .map((e) => e.args.commitment);
+  const tree = new MerkleTree(20, leaves);
+
+  let depositEvent = events.find(
+    (e) => e.args.commitment === toHex(deposit.commitment)
+  );
+  let leafIndex = depositEvent ? depositEvent.args.leafIndex : -1;
+
+  const retVals = await tree.path(leafIndex);
+
+  return retVals;
+}
+
+async function generateSnarkProof(
+  deposit: any,
+  recipient: string,
+  contractAddress: string
+) {
+  // find the inputs that correspond to the path for the deposit
+  const { root, path_elements, path_index } = await generateMerkleProof(
+    deposit,
+    contractAddress
+  );
+
+  let groth16 = await buildGroth16();
+  let circuit = require('./build/circuits/withdraw.json');
+  let proving_key = fs.readFileSync(
+    './build/circuits/withdraw_proving_key.bin'
+  ).buffer;
+
+  // Circuit input
+  const input = {
+    // public
+    root: root,
+    nullifierHash: deposit.nullifierHash,
+    relayer: 0,
+    recipient: snarkjs.bigInt(recipient),
+    fee: 0,
+    refund: 0,
+
+    // private
+    nullifier: deposit.nullifier,
+    secret: deposit.secret,
+    pathElements: path_elements,
+    pathIndices: path_index,
+  };
+
+  const proofData = await websnarkUtils.genWitnessAndProve(
+    groth16,
+    input,
+    circuit,
+    proving_key
+  );
+  const { proof } = websnarkUtils.toSolidityInput(proofData);
+
+  const args = [
+    toHex(input.root),
+    toHex(input.nullifierHash),
+    toHex(input.recipient, 20),
+    toHex(input.relayer, 20),
+    toHex(input.fee),
+    toHex(input.refund),
+  ];
+
+  return { proof, args };
+}
+
+async function startWebbRelayer() {
+  const proc = spawn('../target/debug/webb-relayer', [
+    '-vvvv',
+    '-c',
+    './config.toml',
+  ]);
+  proc.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  proc.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+
+  proc.on('close', (code) => {
+    console.log(`relayer process exited with code ${code}`);
+  });
+
+  return proc;
+}
+
+async function main() {
+  const contractAddress = await deployNativeAnchor();
+  const depositArgs = await deposit(contractAddress);
+  const myAddress = await wallet.getAddress();
+  const relayer = await startWebbRelayer();
+  const client = new WebSocket('ws://localhost:9955');
+  await new Promise((resolve) => client.on('open', resolve));
+  console.log('Connected to Relayer');
+  client.on('message', (data) => {
+    console.log(data);
+  });
+  client.on('error', (err) => {
+    console.log(err);
+    relayer.kill('SIGTERM');
+    client.terminate();
+    process.exit(1);
+  });
+
+  const { proof, args } = await generateSnarkProof(
+    depositArgs,
+    myAddress,
+    contractAddress
+  );
+  const req = {
+    evm: {
+      ganache: {
+        relayWithdrew: {
+          contract: contractAddress,
+          proof,
+          root: args[0],
+          nullifierHash: args[1],
+          recipient: args[2],
+          relayer: args[3],
+          fee: args[4],
+          refund: args[5],
+        },
+      },
+    },
+  };
+  if (client.readyState === client.OPEN) {
+    let data = JSON.stringify(req);
+    console.log(data);
+    client.send(data, (err) => {
+      console.log('!!Error!!', err);
+      relayer.kill('SIGTERM');
+      client.terminate();
+      process.exit(1);
+    });
+    await new Promise((resolve) => setTimeout(resolve, 30_000));
+  } else {
+    console.error('Relayer Connection closed!');
+    relayer.kill();
+    process.exit(1);
+  }
+
+  relayer.kill('SIGTERM');
+  process.exit();
+}
+
+main().catch(console.error);
