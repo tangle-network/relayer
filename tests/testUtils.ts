7b3c30fbbbeb7185d3775d577fba676249b7a8dc
Nathan Barnavon <nepoche@yahoo.com>
2021-07-06 13:41:10 -0400

    Configure tests to run against a multitude of networks

---
+++ tests/testUtils.ts
@@ -0,0 +1,93 @@
+
+export type TestableChain = {
+  endpoint: string,
+  name: string,
+  contractAddress: string,
+};
+
+export const getSupportedChain = (chainName: string): TestableChain => {
+  const chain = supportedChains.find((entry) => chainName == entry.name);
+
+  if (!chain) {
+    throw new Error("Unsupported chain");
+  }
+
+  return chain;
+};
+
+export const generateWithdrawRequest = (chain: TestableChain, proof: string, args: string[]) => {
+  let req;
+
+  if (chain.name == 'beresheet') {
+    req = {
+      evm: {
+        beresheet: {
+          relayWithdrew: {
+            contract: chain.contractAddress,
+            proof,
+            root: args[0],
+            nullifierHash: args[1],
+            recipient: args[2],
+            relayer: args[3],
+            fee: args[4],
+            refund: args[5],
+          },
+        },
+      },
+    };
+  } else if (chain.name == 'harmony') {
+    req = {
+      evm: {
+        harmony: {
+          relayWithdrew: {
+            contract: chain.contractAddress,
+            proof,
+            root: args[0],
+            nullifierHash: args[1],
+            recipient: args[2],
+            relayer: args[3],
+            fee: args[4],
+            refund: args[5],
+          },
+        },
+      },
+    };
+  } else {
+    req = {
+      evm: {
+        ganache: {
+          relayWithdrew: {
+            contract: chain.contractAddress,
+            proof,
+            root: args[0],
+            nullifierHash: args[1],
+            recipient: args[2],
+            relayer: args[3],
+            fee: args[4],
+            refund: args[5],
+          },
+        },
+      },
+    };
+  }
+
+  return req;
+}
+
+const supportedChains: TestableChain[] = [
+  {
+    endpoint: 'http://127.0.0.1:1998',
+    name: 'ganache',
+    contractAddress: '',
+  },
+  {
+    endpoint: 'http://beresheet3.edgewa.re:9933',
+    name: 'beresheet',
+    contractAddress: '0x2ee2e51cab1561E4482cacc8Be8b46CE61E46991',
+  },
+  {
+    endpoint: 'https://api.s1.b.hmny.io',
+    name: 'harmony',
+    contractAddress: '0x59DCE3dcA8f47Da895aaC4Df997d8A2E29815B1B',
+  },
+];
