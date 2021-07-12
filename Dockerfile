647710c3c8ed7b759cc9077d752d2cde76125b54
Shady Khalifa <shekohex@gmail.com>
2021-07-13 01:17:37 +0200

    add docker and CI

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM scratch
+LABEL APP ="Webb Relayer"
+LABEL AUTHOR="Webb Developers <dev@webb.tools>"
+
+ENV RUST_BACKTRACE=full
+ENV WEBB_PORT=9955
+
+ADD build/webb-relayer webb-relayer
+VOLUME [ "/config" ]
+
+EXPOSE ${WEBB_PORT}
+
+CMD ["webb-relayer", "-vvvv", "-c", "/config/config.toml"]
--- ci/build.bash
+++ ci/build.bash
@@ -6,10 +6,12 @@ source ci/common.bash
 
 # $1 {path} = Path to cross/cargo executable
 CROSS=$1
-# $1 {string} = <Target Triple> e.g. x86_64-pc-windows-msvc
+# $2 {string} = <Target Triple> e.g. x86_64-pc-windows-msvc
 TARGET_TRIPLE=$2
 # $3 {boolean} = Are we building for deployment?
 RELEASE_BUILD=$3
+# $4 {boolean} = Are we building for docker?
+RELEASE_DOCKER=$4
 
 required_arg $CROSS 'CROSS'
 required_arg $TARGET_TRIPLE '<Target Triple>'
@@ -19,4 +21,12 @@ if [ -z "$RELEASE_BUILD" ]; then
     $CROSS build --target $TARGET_TRIPLE --all-features
 else
     $CROSS build --target $TARGET_TRIPLE --all-features --release
+fi
+
+if [-z "$RELEASE_DOCKER" ]; then
+    # Do nothing
+else
+    # build for docker
+    $CROSS build --target $TARGET_TRIPLE --all-features --release
+    mkdir -p build && cp target/"$TARGET_TRIPLE"/release/webb-relayer build/webb-relayer
 fi
