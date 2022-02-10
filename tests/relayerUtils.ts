106e67fef6eb91b334f1ed46c55524a72c6544c3
ahmed korim <ahmedkorrim@gmail.com>
2022-02-10 14:01:43 +0200

    Fix macos docker and udpate wasm-utils

--- tests/relayerUtils.ts
+++ tests/relayerUtils.ts
@@ -155,59 +155,67 @@ export const getRelayerConfig = async (
     contracts: relayerInfo.evm[chainName].contracts,
   };
 };
-function spawnWithLogger(command: string, options?: SpawnOptionsWithoutStdio) {
-  const process = spawn(command, options);
-  process.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
+function spawnWithLogger(
+	command: string,
+	options?: SpawnOptionsWithoutStdio,
+	allLogs = false
+) {
+	const process = spawn(command, options);
 
-  process.stderr.on('data', (data) => {
-    console.error(data.toString());
-  });
+	if (allLogs) {
+		process.stdout.on('data', (data) => {
+			console.log(data.toString());
+		});
 
-  process.on('close', (code) => {
-    console.log(` process ${process.pid} exited with code ${code}`);
-  });
+		process.stderr.on('data', (data) => {
+			console.error(data.toString());
+		});
+	}
 
-  return process;
+	process.on('close', (code) => {
+		console.log(` process ${process.pid} exited with code ${code}`);
+	});
+
+	return process;
 }
 export type KillTask = () => void
 export function startDarkWebbNode(): KillTask {
-  execSync(
-    'docker pull ghcr.io/webb-tools/protocol-substrate-standalone-node:edge',
-    { stdio: 'inherit' }
-  );
-  const node1 =
-    'webb-standalone-node  --dev --alice --node-key 0000000000000000000000000000000000000000000000000000000000000001 --ws-port=9944 --rpc-cors all';
-  const node2 =
-    'webb-standalone-node --dev --bob --port 33334 --tmp --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp --ws-port=9999';
+	execSync(
+		'docker pull ghcr.io/webb-tools/protocol-substrate-standalone-node:edge',
+		{ stdio: 'inherit' }
+	);
+	const DOCKER_NETWORK_NAME = 'webb-network'
+	try{
+		execSync(`docker network create -d bridge ${DOCKER_NETWORK_NAME}`)
+	}catch (e) {
+		console.log((e as any)?.toString());
+	}
+	const node1 =
+		'webb-standalone-node  --dev --alice --node-key 0000000000000000000000000000000000000000000000000000000000000001 --ws-port=9944 --rpc-cors all --ws-external';
+	const node2 =
+		'webb-standalone-node --dev --bob --port 33334 --tmp --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp --ws-port=9993';
 
-  const getDockerCmd = (cmd: string, ports: number[]) => {
-    return `docker run --network host  --rm ${ports.reduce(
-      (acc, port) => `${acc} -p ${port}:${port}`,
-      ''
-    )} ghcr.io/webb-tools/protocol-substrate-standalone-node:edge  ${cmd}`;
-  };
+	const getDockerCmd = (cmd: string, ports: number[]) => {
+		return `docker run --network ${DOCKER_NETWORK_NAME}  --rm ${ports.reduce(
+			(acc, port) => `${acc} -p ${port}:${port}`,
+			''
+		)} ghcr.io/webb-tools/protocol-substrate-standalone-node:edge  ${cmd}`;
+	};
 
-  const node1Cmd = getDockerCmd(node1, [9944, 30333]);
-  const node2Cmd = getDockerCmd(node2 ,       [33334, 33334]);
+	const node1Cmd = getDockerCmd(node1, [9944, 30333]);
+	const node2Cmd = getDockerCmd(node2, [33334, 33334]);
 
-  const node1Task = spawnWithLogger(
-      node1Cmd,
-    {
-      shell: true,
-    }
-  );
-  const node2task = spawnWithLogger(
-      node2Cmd,
-      {
-        shell: true,
-      }
-  );
-  return ()=>{
-    node1Task.kill("SIGINT")
-    node2task.kill("SIGINT")
-  };
+	const node1Task = spawnWithLogger(node1Cmd, {
+		shell: true,
+	});
+	const node2task = spawnWithLogger(node2Cmd, {
+		shell: true,
+	});
+	return () => {
+		node1Task.kill('SIGINT');
+		node2task.kill('SIGINT');
+		execSync(`docker network rm -f ${DOCKER_NETWORK_NAME}`)
+	};
 }
 
 export function startWebbRelayer(
