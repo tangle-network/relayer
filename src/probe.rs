b865b394f2c36897129791bede068a728174c0b5
Shady Khalifa <shekohex@gmail.com>
2022-02-23 17:52:26 +0200

    Webb Relayer Probe (#105)

---
+++ src/probe.rs
@@ -0,0 +1,13 @@
+use derive_more::Display;
+pub const TARGET: &str = "webb_probe";
+
+/// The Kind of the Probe.
+#[derive(Debug, Display, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Kind {
+    /// When the Lifecycle of the Relayer changes, like starting or shutting down.
+    #[display(fmt = "lifecycle")]
+    Lifecycle,
+    /// Relayer Sync state on a specific chain/node.
+    #[display(fmt = "sync")]
+    Sync,
+}
