87599c540a7575848dfc0d0b6eeacfe8e174adc9
Shady Khalifa <shekohex@gmail.com>
2022-03-24 19:10:32 +0200

    Get typescript compiler working with mocha (#110)
    Co-authored-by: nepoche <nepoche@yahoo.com>

--- tests/babel.config.js
+++ tests/babel.config.js
@@ -1,8 +1,8 @@
-module.exports = function (api) {
+export default function (api) {
   // Cache configuration is a required option
   api.cache(false);
 
   const presets = ['@babel/preset-typescript', '@babel/preset-env'];
 
   return { presets };
-};
+}
