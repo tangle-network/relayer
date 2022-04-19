79ef990a7cf4cab63932fbb9abff04dafb161c30
Shady Khalifa <shekohex@gmail.com>
2022-04-19 18:43:09 +0200

    it shall work now

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -178,6 +178,24 @@ pub trait HistoryStore: Clone + Send + Sync {
     }
 }
 
+/// A Simple Event Store, that does not store the events, instead it store the hash of the event as the key
+/// and the value is just empty bytes.
+///
+/// This is mainly useful to mark the event as processed.
+pub trait EventHashStore: Send + Sync + Clone {
+    /// Store the event in the store.
+    /// the key is the hash of the event.
+    fn store_event(&self, event: &[u8]) -> anyhow::Result<()>;
+
+    /// Check if the event is stored in the store.
+    /// the key is the hash of the event.
+    fn contains_event(&self, event: &[u8]) -> anyhow::Result<bool>;
+
+    /// Delete the event from the store.
+    /// the key is the hash of the event.
+    fn delete_event(&self, event: &[u8]) -> anyhow::Result<()>;
+}
+
 /// A Leaf Cache Store is a simple trait that would help in
 /// getting the leaves and insert them with a simple API.
 pub trait LeafCacheStore: HistoryStore {
