8be579abac8258f18e5ebcc796fc691d74c18dc1
Damilare <dharjeezy@gmail.com>
2022-05-08 00:28:04 +0100

    check for proof errors

--- tests/test/substrate/mixerTransactionRelayer.test.ts
+++ tests/test/substrate/mixerTransactionRelayer.test.ts
@@ -185,7 +185,9 @@ describe('Substrate Mixer Transaction Relayer', function () {
       // Expect an error to be thrown
       expect(e).to.not.be.null;
       // Runtime Error that indicates invalid withdrawal proof
-      expect(e).to.contain('Module { index: 40, error: 1 }');
+      expect(e).to.contain('Module { index: 35, error: 1 }') || expect(e).to.contain(
+          'Runtime error: RuntimeError(Module { index: 40, error: 1 }'
+      );
     }
   });
 
