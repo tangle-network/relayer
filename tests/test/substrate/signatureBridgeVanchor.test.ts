f9ff86484911e7fa7900724c4c1b5b9f2ab9254c
salman01zp <pathansalman555@gmail.com>
2022-09-21 19:09:31 +0530

    remove only test

--- tests/test/substrate/signatureBridgeVanchor.test.ts
+++ tests/test/substrate/signatureBridgeVanchor.test.ts
@@ -58,7 +58,7 @@ import pkg from 'secp256k1';
 import { makeSubstrateTargetSystem } from '../../lib/webbProposals.js';
 const { ecdsaSign } = pkg;
 
-describe.only('Substrate Signature Bridge Relaying On Vanchor Deposit <<>> Mocked Backend', function () {
+describe('Substrate Signature Bridge Relaying On Vanchor Deposit <<>> Mocked Backend', function () {
   const tmpDirPath = temp.mkdirSync();
   let aliceNode: LocalProtocolSubstrate;
   let bobNode: LocalProtocolSubstrate;
