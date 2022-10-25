bfe9ffc93665dd76113c9ad2fc29acb687445efc
drewstone <drewstone329@gmail.com>
2022-10-25 07:41:59 -0600

    Update crates/event-watcher-traits/src/substrate/mod.rs
    Co-authored-by: shekohex <dev+github@shadykhalifa.me>

--- crates/event-watcher-traits/src/substrate/mod.rs
+++ crates/event-watcher-traits/src/substrate/mod.rs
@@ -37,7 +37,7 @@ use webb_relayer_store::{
 use self::event_watcher::SubstrateEventWatcher;
 
 /// Event watching traits
-pub mod event_watcher;
+mod event_watcher;
 pub use event_watcher::*;
 
 /// Bridge watching traits
