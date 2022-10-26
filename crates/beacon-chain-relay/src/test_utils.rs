52440550273fe44e449d46d0d8155b748d93d1a1
Drew Stone <drewstone329@gmail.com>
2022-10-25 21:46:24 -0600

    Fix

--- crates/beacon-chain-relay/src/test_utils.rs
+++ crates/beacon-chain-relay/src/test_utils.rs
@@ -1,9 +1,11 @@
 pub fn read_json_file_from_data_dir(file_name: &str) -> std::string::String {
     let mut json_file_path = std::env::current_exe().unwrap();
 
-    json_file_path.pop();
-    json_file_path.push("../../../crates/beacon-chain-relay/data");
+    while json_file_path.clone().into_os_string().into_string().unwrap().contains("target".to_string().as_str()) {
+        json_file_path.pop();
+    }
+
+    json_file_path.push("./crates/beacon-chain-relay/data");
     json_file_path.push(file_name);
-    println!("json_file_path: {json_file_path:?}");
     std::fs::read_to_string(json_file_path).expect("Unable to read file")
 }
