c25d313e9b1e3d40f74281eb42e64bf589a6d65a
Drew Stone <drewstone329@gmail.com>
2022-10-25 21:48:15 -0600

    Fmt

--- crates/beacon-chain-relay/src/test_utils.rs
+++ crates/beacon-chain-relay/src/test_utils.rs
@@ -1,7 +1,13 @@
 pub fn read_json_file_from_data_dir(file_name: &str) -> std::string::String {
     let mut json_file_path = std::env::current_exe().unwrap();
 
-    while json_file_path.clone().into_os_string().into_string().unwrap().contains("target".to_string().as_str()) {
+    while json_file_path
+        .clone()
+        .into_os_string()
+        .into_string()
+        .unwrap()
+        .contains("target".to_string().as_str())
+    {
         json_file_path.pop();
     }
 
