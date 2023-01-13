10d80b63c293d077499451f058fedcd2705a50b2
Felix Ableitner <felix@commonwealth.im>
2023-01-13 12:32:44 +0100

    Add caching for FeeInfo, write test code (currently failing)

--- crates/relayer-handlers/src/routes/fees.rs
+++ crates/relayer-handlers/src/routes/fees.rs
@@ -1,40 +1,15 @@
 use ethereum_types::U256;
-use serde::Serialize;
-use webb_relayer_utils::fees::max_refund;
-use webb_relayer_utils::fees::{
-    calculate_exchange_rate, calculate_wrapped_fee,
-};
-
-/// Return value of fee_info API call. Contains information about relay transaction fee and refunds.
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct FeeInfo {
-    /// Estimated fee for an average relay transaction, in `wrappedToken`
-    /// TODO: We dont know the actual gas amount of the transaction here, so it would make more
-    ///       sense to return a gas price, and then calculate the fee on the client using estimated
-    ///       gas amount. Then we could remove the hardcoded `estimated_gas_amount`.
-    fee: U256,
-    /// Exchange rate for refund from `wrappedToken` to `nativeToken`
-    refund_exchange_rate: f64,
-    /// Maximum amount of `wrappedToken` which can be exchanged to `nativeToken` by relay
-    max_refund: U256,
-}
+use webb_relayer_utils::fees::{get_fee_info, FeeInfo};
 
 /// Calculate fee for an average transaction over the relay. Also returns information about refund.
 pub async fn calculate_fees() -> webb_relayer_utils::Result<FeeInfo> {
     // TODO: hardcoded
     let estimated_gas_amount = U256::from(1_721_713);
-
     // TODO: need to get the actual tokens which are being exchanged (also in handle_vanchor_relay_tx)
-    let exchange_rate = calculate_exchange_rate("usd-coin", "ethereum").await;
+    let wrapped_token = "usd-coin";
+    let base_token = "ethereum";
 
-    let wrapped_fee =
-        calculate_wrapped_fee(estimated_gas_amount, exchange_rate).await;
-    let max_refund = max_refund("usd-coin").await;
-
-    Ok(FeeInfo {
-        fee: wrapped_fee,
-        refund_exchange_rate: exchange_rate,
-        max_refund,
-    })
+    let fee_info =
+        get_fee_info(estimated_gas_amount, wrapped_token, base_token).await;
+    Ok(fee_info)
 }
--- crates/relayer-utils/src/fees.rs
+++ crates/relayer-utils/src/fees.rs
@@ -1,27 +1,91 @@
+use chrono::DateTime;
+use chrono::Duration;
+use chrono::Utc;
 use coingecko::CoinGeckoClient;
 use ethers::etherscan;
 use ethers::types::Chain;
 use once_cell::sync::Lazy;
+use serde::Serialize;
+use std::ops::Add;
+use std::sync::Mutex;
 use webb::evm::ethers::prelude::U256;
 
 /// Maximum refund amount per relay transaction in USD.
 const MAX_REFUND_USD: f64 = 1.;
-
+/// Amount of time for which a `FeeInfo` is valid after creation
+const FEE_CACHE_TIME: Lazy<Duration> = Lazy::new(|| Duration::minutes(1));
 /// Number of digits after the comma of USD-Coin.
 const USDC_DECIMALS: u32 = 6;
 
 static COIN_GECKO_CLIENT: Lazy<CoinGeckoClient> =
     Lazy::new(CoinGeckoClient::default);
-
 static ETHERSCAN_CLIENT: Lazy<etherscan::Client> =
     Lazy::new(|| etherscan::Client::new_from_env(Chain::Mainnet).unwrap());
+/// Fee info which was previously generated. It is still valid if `timestamp` is no older than
+/// `FEE_CACHE_TIME`.
+static FEE_INFO_CACHED: Mutex<Option<FeeInfo>> = Mutex::new(None);
+
+/// Return value of fee_info API call. Contains information about relay transaction fee and refunds.
+#[derive(Debug, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct FeeInfo {
+    /// Estimated fee for an average relay transaction, in `wrappedToken`. This is only for
+    /// display to the user
+    estimated_fee: U256,
+    /// Price per gas using "normal" confirmation speed, in `nativeToken`
+    pub gas_price: U256,
+    /// Exchange rate for refund from `wrappedToken` to `nativeToken`
+    pub refund_exchange_rate: f64,
+    /// Maximum amount of `wrappedToken` which can be exchanged to `nativeToken` by relay
+    pub max_refund: U256,
+    timestamp: DateTime<Utc>,
+}
+
+/// Get the current fee info.
+///
+/// If fee info was recently requested, the cached value is used. Otherwise it is regenerated
+/// based on the current exchange rate and estimated gas price.
+pub async fn get_fee_info(
+    estimated_gas_amount: U256,
+    wrapped_token: &str,
+    base_token: &str,
+) -> FeeInfo {
+    // Check if there is an existing, recent fee info. Return it directly if thats the case.
+    {
+        let lock = FEE_INFO_CACHED.lock().unwrap();
+        if let Some(fee_info) = &*lock {
+            let fee_info_valid_time = fee_info.timestamp.add(*FEE_CACHE_TIME);
+            if fee_info_valid_time > Utc::now() {
+                return fee_info.clone();
+            }
+        }
+    }
+    let exchange_rate =
+        calculate_exchange_rate(wrapped_token, base_token).await;
+
+    let gas_price = estimate_gas_price().await;
+    let wrapped_fee =
+        calculate_wrapped_fee(gas_price, estimated_gas_amount, exchange_rate)
+            .await;
+    let max_refund = max_refund(wrapped_token).await;
+
+    let fee_info = FeeInfo {
+        estimated_fee: wrapped_fee,
+        gas_price,
+        refund_exchange_rate: exchange_rate,
+        max_refund,
+        timestamp: Utc::now(),
+    };
+    *FEE_INFO_CACHED.lock().unwrap() = Some(fee_info.clone());
+    fee_info
+}
 
 /// Calculate fee in `wrappedToken`, using the estimated gas price from etherscan.
 pub async fn calculate_wrapped_fee(
+    gas_price: U256,
     estimated_gas_amount: U256,
     exchange_rate: f64,
 ) -> U256 {
-    let gas_price = U256::from(estimate_gas_price().await.unwrap());
     let native_fee = (gas_price * estimated_gas_amount).as_u128() as f64;
     let wrapped_fee = native_fee * exchange_rate;
     to_u256(wrapped_fee)
@@ -29,10 +93,7 @@ pub async fn calculate_wrapped_fee(
 
 /// Pull USD prices of wrapped token and base token from coingecko.com, and use these to
 /// calculate the exchange rate.
-pub async fn calculate_exchange_rate(
-    wrapped_token: &str,
-    base_token: &str,
-) -> f64 {
+async fn calculate_exchange_rate(wrapped_token: &str, base_token: &str) -> f64 {
     let tokens = &[wrapped_token, base_token];
     let prices = COIN_GECKO_CLIENT
         .price(tokens, &["usd"], false, false, false, false)
@@ -45,15 +106,15 @@ pub async fn calculate_exchange_rate(
 
 /// Estimate gas price using etherscan.io. Note that this functionality is only available
 /// on mainnet.
-async fn estimate_gas_price() -> crate::Result<u64> {
+async fn estimate_gas_price() -> U256 {
     let gas_oracle = ETHERSCAN_CLIENT.gas_oracle().await.unwrap();
     // use the "average" gas price
-    Ok(gas_oracle.propose_gas_price)
+    U256::from(gas_oracle.propose_gas_price)
 }
 
 /// Calculate the maximum refund amount per relay transaction in `wrappedToken`, based on
 /// `MAX_REFUND_USD`.
-pub async fn max_refund(wrapped_token: &str) -> U256 {
+async fn max_refund(wrapped_token: &str) -> U256 {
     let prices = COIN_GECKO_CLIENT
         .price(&[wrapped_token], &["usd"], false, false, false, false)
         .await
