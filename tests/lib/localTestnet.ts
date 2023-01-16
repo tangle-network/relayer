54b1785074aee4b7046c2941e780c26a7566d52e
Felix Ableitner <felix@commonwealth.im>
2023-01-16 15:41:43 +0100

    Test working

--- tests/lib/localTestnet.ts
+++ tests/lib/localTestnet.ts
@@ -498,12 +498,12 @@ export async function setupVanchorEvmTx(
   relayerWallet2: Wallet,
   tokenAddress: string,
   fee: BigNumberish,
-  refund: BigNumberish,
+  refund: BigNumberish
 ): Promise<{
   extData: IVariableAnchorExtData;
   publicInputs: IVariableAnchorPublicInputs;
 }> {
-  const extAmount = ethers.BigNumber.from(0).sub(depositUtxo.amount);
+  const extAmount = ethers.BigNumber.from(0).sub(depositUtxo.amount).add(fee);
 
   const dummyOutput1 = await CircomUtxo.generateUtxo({
     curve: 'Bn254',
