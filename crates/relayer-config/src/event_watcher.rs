3f40c622b5b2295ebe79a7bf2741dcd73daf5295
shekohex <dev+github@shadykhalifa.me>
2023-04-03 15:51:27 +0200

    Rename Fields with serde while serialization to be `camelCase`

--- crates/relayer-config/src/event_watcher.rs
+++ crates/relayer-config/src/event_watcher.rs
@@ -5,7 +5,10 @@ use super::*;
 #[serde(rename_all = "kebab-case")]
 pub struct EventsWatcherConfig {
     /// A flag for enabling API endpoints for querying data from the relayer.
-    #[serde(default = "enable_data_query_default")]
+    #[serde(
+        default = "enable_data_query_default",
+        rename(serialize = "enableDataQuery")
+    )]
     pub enable_data_query: bool,
     #[serde(default = "enable_leaves_watcher_default")]
     /// if it is enabled for this chain or not.
@@ -14,7 +17,11 @@ pub struct EventsWatcherConfig {
     #[serde(rename(serialize = "pollingInterval"))]
     pub polling_interval: u64,
     /// The maximum number of events to fetch in one request.
-    #[serde(skip_serializing, default = "max_blocks_per_step_default")]
+    #[serde(
+        skip_serializing,
+        default = "max_blocks_per_step_default",
+        rename(serialize = "maxBlocksPerStep")
+    )]
     pub max_blocks_per_step: u64,
     /// print sync progress frequency in milliseconds
     /// if it is zero, means no progress will be printed.
