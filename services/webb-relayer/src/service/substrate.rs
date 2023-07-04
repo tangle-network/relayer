ea2f29944caba375582a4691f3b6215a8d1aab9b
salman01zp <pathansalman555@gmail.com>
2023-07-04 16:38:25 +0530

    remove substrate watchers

--- services/webb-relayer/src/service/substrate.rs
+++ services/webb-relayer/src/service/substrate.rs
@@ -5,23 +5,13 @@ use axum::Router;
 use sp_core::sr25519;
 use webb::substrate::subxt::config::ExtrinsicParams;
 use webb::substrate::subxt::{self, PolkadotConfig};
-use webb_bridge_registry_backends::dkg::DkgBridgeRegistryBackend;
-use webb_bridge_registry_backends::mocked::MockedBridgeRegistryBackend;
-use webb_event_watcher_traits::{
-    SubstrateBridgeWatcher, SubstrateEventWatcher,
-};
+use webb_event_watcher_traits::SubstrateEventWatcher;
 use webb_ew_dkg::{
     DKGMetadataWatcher, DKGProposalHandlerWatcher, DKGPublicKeyChangedHandler,
     ProposalSignedHandler,
 };
-use webb_ew_substrate::{
-    MaintainerSetEventHandler, SubstrateBridgeEventWatcher,
-    SubstrateVAnchorDepositHandler, SubstrateVAnchorEncryptedOutputHandler,
-    SubstrateVAnchorEventWatcher, SubstrateVAnchorLeavesHandler,
-};
 use webb_relayer_config::substrate::{
-    DKGPalletConfig, DKGProposalHandlerPalletConfig, Pallet,
-    SignatureBridgePalletConfig, SubstrateConfig, VAnchorBn254PalletConfig,
+    DKGPalletConfig, DKGProposalHandlerPalletConfig, Pallet, SubstrateConfig
 };
 use webb_relayer_context::RelayerContext;
 use webb_relayer_handlers::routes::fee_info::handle_substrate_fee_info;
@@ -29,8 +19,6 @@ use webb_relayer_handlers::routes::{
     leaves, metric, private_tx_withdraw, transaction_status,
 };
 use webb_relayer_tx_queue::substrate::SubstrateTxQueue;
-
-use super::ProposalSigningBackendSelector;
 
 /// Type alias for the Tangle DefaultConfig
 pub type TangleClient = subxt::OnlineClient<PolkadotConfig>;
@@ -108,27 +96,11 @@ async fn ignite_tangle_runtime(
             Pallet::DKGProposals(_) => {
                 // TODO(@shekohex): start the dkg proposals service
             }
-            Pallet::SignatureBridge(config) => {
-                start_substrate_signature_bridge_events_watcher(
-                    ctx.clone(),
-                    config,
-                    chain_id,
-                    store.clone(),
-                )
-                .await?;
-            }
-            Pallet::VAnchorBn254(config) => {
-                start_substrate_vanchor_event_watcher(
-                    ctx.clone(),
-                    config,
-                    chain_id,
-                    store.clone(),
-                )?;
-            }
+            _=> { unimplemented!()}
         }
     }
     // start the transaction queue for dkg-substrate extrinsics after starting other tasks.
-    start_tx_queue::<PolkadotConfig>(ctx.clone(), chain_id, store.clone())?;
+    start_tx_queue::<PolkadotConfig>(ctx, chain_id, store)?;
     Ok(())
 }
 
@@ -248,240 +220,6 @@ pub fn start_dkg_pallet_watcher(
             _ = shutdown_signal.recv() => {
                 tracing::trace!(
                     "Stopping DKG Pallet events watcher for ({})",
-                    chain_id,
-                );
-            },
-        }
-    };
-    // kick off the watcher.
-    tokio::task::spawn(task);
-    Ok(())
-}
-
-/// Starts the event watcher for Substrate vanchor events.
-///
-/// Returns Ok(()) if successful, or an error if not.
-///
-/// # Arguments
-///
-/// * `ctx` - RelayContext reference that holds the configuration
-/// * `config` - VAnchorBn254 configuration
-/// * `client` - WebbProtocol client
-/// * `chain_id` - An u32 representing the chain id of the chain
-/// * `store` -[Sled](https://sled.rs)-based database store
-pub fn start_substrate_vanchor_event_watcher(
-    ctx: RelayerContext,
-    config: &VAnchorBn254PalletConfig,
-    chain_id: u32,
-    store: Arc<super::Store>,
-) -> crate::Result<()> {
-    if !config.events_watcher.enabled {
-        tracing::warn!(
-            "Substrate VAnchor events watcher is disabled for ({}).",
-            chain_id,
-        );
-        return Ok(());
-    }
-    tracing::debug!(
-        "Substrate VAnchor events watcher for ({}) Started.",
-        chain_id,
-    );
-    let my_ctx = ctx.clone();
-    let my_config = config.clone();
-    let mut shutdown_signal = ctx.shutdown_signal();
-    let metrics = ctx.metrics.clone();
-    let task = async move {
-        let proposal_signing_backend = super::make_proposal_signing_backend(
-            &my_ctx,
-            store.clone(),
-            webb_proposals::TypedChainId::Substrate(chain_id),
-            my_config.linked_anchors.clone(),
-            my_config.proposal_signing_backend,
-        )
-        .await?;
-        match proposal_signing_backend {
-            ProposalSigningBackendSelector::Dkg(backend) => {
-                let bridge_registry =
-                    DkgBridgeRegistryBackend::new(backend.client.clone());
-
-                let deposit_handler = SubstrateVAnchorDepositHandler::new(
-                    backend,
-                    bridge_registry,
-                    my_config.linked_anchors,
-                );
-                let leaves_handler = SubstrateVAnchorLeavesHandler::default();
-                let encrypted_output_handler =
-                    SubstrateVAnchorEncryptedOutputHandler::default();
-
-                let watcher = SubstrateVAnchorEventWatcher::default();
-                let substrate_vanchor_watcher_task = watcher.run(
-                    chain_id,
-                    ctx.clone(),
-                    store.clone(),
-                    my_config.events_watcher,
-                    vec![
-                        Box::new(deposit_handler),
-                        Box::new(leaves_handler),
-                        Box::new(encrypted_output_handler),
-                    ],
-                    metrics.clone(),
-                );
-
-                tokio::select! {
-                    _ = substrate_vanchor_watcher_task => {
-                        tracing::warn!(
-                            "Substrate VAnchor watcher (DKG Backend) task stopped for ({})",
-                            chain_id,
-                        );
-                    },
-
-                    _ = shutdown_signal.recv() => {
-                        tracing::trace!(
-                            "Stopping Substrate VAnchor watcher (DKG Backend) for ({})",
-                            chain_id,
-                        );
-                    },
-                }
-            }
-            ProposalSigningBackendSelector::Mocked(backend) => {
-                let bridge_registry =
-                    MockedBridgeRegistryBackend::builder().build();
-
-                let deposit_handler = SubstrateVAnchorDepositHandler::new(
-                    backend,
-                    bridge_registry,
-                    my_config.linked_anchors,
-                );
-                let leaves_handler = SubstrateVAnchorLeavesHandler::default();
-                let encrypted_output_handler =
-                    SubstrateVAnchorEncryptedOutputHandler::default();
-
-                let watcher = SubstrateVAnchorEventWatcher::default();
-                let substrate_vanchor_watcher_task = watcher.run(
-                    chain_id,
-                    ctx.clone(),
-                    store.clone(),
-                    my_config.events_watcher,
-                    vec![
-                        Box::new(deposit_handler),
-                        Box::new(leaves_handler),
-                        Box::new(encrypted_output_handler),
-                    ],
-                    metrics.clone(),
-                );
-                tokio::select! {
-                    _ = substrate_vanchor_watcher_task => {
-                        tracing::warn!(
-                            "Substrate VAnchor watcher (Mocked Backend) task stopped for ({})",
-                            chain_id,
-                        );
-                    },
-                    _ = shutdown_signal.recv() => {
-                        tracing::trace!(
-                            "Stopping Substrate VAnchor watcher (Mocked Backend) for ({})",
-                            chain_id,
-                        );
-                    },
-                }
-            }
-            ProposalSigningBackendSelector::None => {
-                let leaves_handler = SubstrateVAnchorLeavesHandler::default();
-                let encrypted_output_handler =
-                    SubstrateVAnchorEncryptedOutputHandler::default();
-
-                let watcher = SubstrateVAnchorEventWatcher::default();
-                let substrate_vanchor_watcher_task = watcher.run(
-                    chain_id,
-                    ctx.clone(),
-                    store.clone(),
-                    my_config.events_watcher,
-                    vec![
-                        Box::new(leaves_handler),
-                        Box::new(encrypted_output_handler),
-                    ],
-                    metrics.clone(),
-                );
-                tokio::select! {
-                    _ = substrate_vanchor_watcher_task => {
-                        tracing::warn!(
-                            "Substrate VAnchor watcher task stopped for ({})",
-                            chain_id,
-                        );
-                    },
-                    _ = shutdown_signal.recv() => {
-                        tracing::trace!(
-                            "Stopping Substrate VAnchor watcher task for ({})",
-                            chain_id,
-                        );
-                    },
-                }
-            }
-        };
-
-        tracing::debug!("Task resturned");
-        crate::Result::Ok(())
-    };
-    // kick off the watcher.
-    tokio::task::spawn(task);
-    Ok(())
-}
-
-/// Starts the event watcher for Signature Bridge Pallet.
-pub async fn start_substrate_signature_bridge_events_watcher(
-    ctx: RelayerContext,
-    config: &SignatureBridgePalletConfig,
-    chain_id: u32,
-    store: Arc<super::Store>,
-) -> crate::Result<()> {
-    if !config.events_watcher.enabled {
-        tracing::warn!(
-            "Substrate Signature Bridge events watcher is disabled for ({}).",
-            chain_id,
-        );
-        return Ok(());
-    }
-    let mut shutdown_signal = ctx.shutdown_signal();
-    let my_config = config.clone();
-    let pair = ctx.substrate_wallet(chain_id).await?;
-    let task = async move {
-        tracing::debug!(
-            "Substrate Signature Bridge watcher for ({}) Started.",
-            chain_id
-        );
-        let substrate_bridge_watcher = SubstrateBridgeEventWatcher::default();
-        let bridge_event_handler = MaintainerSetEventHandler::default();
-        let events_watcher_task = SubstrateEventWatcher::run(
-            &substrate_bridge_watcher,
-            chain_id,
-            ctx.clone(),
-            store.clone(),
-            my_config.events_watcher,
-            vec![Box::new(bridge_event_handler)],
-            ctx.metrics.clone(),
-        );
-        let cmd_handler_task = SubstrateBridgeWatcher::run(
-            &substrate_bridge_watcher,
-            chain_id,
-            ctx.clone(),
-            pair.clone(),
-            store.clone(),
-        );
-        tokio::select! {
-            _ = events_watcher_task => {
-                tracing::warn!(
-                    "Substrate signature bridge events watcher task stopped for ({})",
-                    chain_id
-                );
-            },
-            _ = cmd_handler_task => {
-                tracing::warn!(
-                    "Substrate signature bridge cmd handler task stopped for ({})",
-                    chain_id
-                );
-            },
-            _ = shutdown_signal.recv() => {
-                tracing::trace!(
-                    "Stopping Substrate Signature Bridge watcher for ({})",
                     chain_id,
                 );
             },
