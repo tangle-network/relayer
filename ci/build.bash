369a2cf586fd4cc76f2fe96b21e0b14f93942de6
Shady Khalifa <dev+github@shadykhalifa.me>
2023-07-04 14:28:32 +0300

    fix the build script in the CI to only build the relayer

--- ci/build.bash
+++ ci/build.bash
@@ -17,10 +17,9 @@ required_arg $CROSS 'CROSS'
 required_arg $TARGET_TRIPLE '<Target Triple>'
 
 if [ -z "$RELEASE_BUILD" ]; then
-    $CROSS build --target $TARGET_TRIPLE
-    $CROSS build --target $TARGET_TRIPLE --all-features
+    $CROSS build -p webb-relayer --target "$TARGET_TRIPLE" --features cli
 else
-    $CROSS build --target $TARGET_TRIPLE --all-features --release
+    $CROSS build -p webb-relayer --target "$TARGET_TRIPLE" --features cli --release
 fi
 
 if [ -z "$RELEASE_DOCKER" ]; then
