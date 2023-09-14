b6ce678159b93028a48e99a43c79261aa2a7ada9
salman01zp <pathansalman555@gmail.com>
2023-09-14 17:09:28 +0530

    update import paths

--- crates/relayer-types/src/rpc_client.rs
+++ crates/relayer-types/src/rpc_client.rs
@@ -1,10 +1,11 @@
-use std::sync::Arc;
-
-pub use jsonrpsee::core::client::Client;
+use jsonrpsee::async_client::ClientBuilder;
+use jsonrpsee::client_transport::ws::WsTransportClientBuilder;
+use jsonrpsee::core::client::Client;
+use jsonrpsee::core::JsonRawValue;
 use webb::substrate::subxt::{self, rpc::RpcClientT};
 
-#[derive(Debug, Clone)]
-pub struct WebbRpcClient(pub Arc<jsonrpsee::async_client::Client>);
+#[derive(Debug)]
+pub struct WebbRpcClient(pub Client);
 
 impl WebbRpcClient {
     pub async fn new(
@@ -13,20 +14,18 @@ impl WebbRpcClient {
         let url: http::Uri = url.into().parse().map_err(|_| {
             webb_relayer_utils::Error::Generic("RPC url is invalid")
         })?;
-        let (sender, receiver) =
-            jsonrpsee::client_transport::ws::WsTransportClientBuilder::default(
-            )
+        let (sender, receiver) = WsTransportClientBuilder::default()
             .build(url)
             .await
             .map_err(|_| {
                 webb_relayer_utils::Error::Generic("RPC failed to connect")
             })?;
 
-        let client = jsonrpsee::async_client::ClientBuilder::default()
+        let client = ClientBuilder::default()
             .max_notifs_per_subscription(4096)
             .build_with_tokio(sender, receiver);
 
-        Ok(Self(Arc::new(client)))
+        Ok(Self(client))
     }
 }
 
@@ -34,15 +33,15 @@ impl RpcClientT for WebbRpcClient {
     fn request_raw<'a>(
         &'a self,
         method: &'a str,
-        params: Option<Box<jsonrpsee::core::JsonRawValue>>,
-    ) -> subxt::rpc::RpcFuture<'a, Box<jsonrpsee::core::JsonRawValue>> {
+        params: Option<Box<JsonRawValue>>,
+    ) -> subxt::rpc::RpcFuture<'a, Box<JsonRawValue>> {
         self.0.request_raw(method, params)
     }
 
     fn subscribe_raw<'a>(
         &'a self,
         sub: &'a str,
-        params: Option<Box<jsonrpsee::core::JsonRawValue>>,
+        params: Option<Box<JsonRawValue>>,
         unsub: &'a str,
     ) -> subxt::rpc::RpcFuture<'a, subxt::rpc::RpcSubscription> {
         self.0.subscribe_raw(sub, params, unsub)
