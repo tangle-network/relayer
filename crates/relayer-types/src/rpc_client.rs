7257599f357c25c903d58fd3e0a3e2dcc08d357d
salman01zp <pathansalman555@gmail.com>
2024-02-27 22:22:11 +0530

    update license

--- crates/relayer-types/src/rpc_client.rs
+++ crates/relayer-types/src/rpc_client.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2022-2024 Webb Technologies Inc.
+//
+// Tangle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tangle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should receive a copy of the GNU General Public License
+// If not, see <http://www.gnu.org/licenses/>.
+
 use jsonrpsee::async_client::ClientBuilder;
 use jsonrpsee::client_transport::ws::WsTransportClientBuilder;
 use jsonrpsee::core::client::Client;
--- crates/tx-relay/src/evm/mod.rs
+++ crates/tx-relay/src/evm/mod.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2022-2024 Webb Technologies Inc.
+//
+// Tangle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tangle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should receive a copy of the GNU General Public License
+// If not, see <http://www.gnu.org/licenses/>.
+
 use ethereum_types::U256;
 use webb::evm::ethers;
 
